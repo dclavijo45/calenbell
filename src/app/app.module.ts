0e6c065c5987712185cb46264043ed9a35f521b4
Daniel Clavijo Gonzalez <danielclavijo19380@gmail.com>
2021-03-15 23:31:57 +0000

    Agregado validaciones del login y registro y mejoras

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
@@ -16,6 +16,8 @@ import { FooterComponent } from './components/footer/footer.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import * as M from 'node_modules/materialize-css/js/materialize.min.js'
 
 
@@ -10,26 +11,36 @@ import * as M from 'node_modules/materialize-css/js/materialize.min.js'
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
-  public login: boolean = false;
+  constructor(public formB: FormBuilder) { }
 
-  toggle(el,classname){
-    if(el.classList.contains(classname)){
-    el.classList.remove(classname)
-    }
-    else{
-    el.classList.add(classname)
-    }
-  }
-  // $(".info-item .btn").click(function () {
-  //           $(".container").toggleClass("log-in");
-  //       });
-  //       $(".container-form .btn").click(function () {
-  //           $(".container").addClass("active");
-  //       });
+  public login: boolean = false;
+  public success: boolean = false;
+  public loading: boolean = false;
+  public error: boolean = false;
+  public formLogin: FormGroup;
+  public formRegister: FormGroup;
 
   ngOnInit(): void {
     M.AutoInit();
+    M.updateTextFields();
+    this.formLogin = this.formB.group({
+      user: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+    this.formRegister = this.formB.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      user: ['', Validators.required],
+      password: ['', Validators.required]
+    });
   }
 
+  loginUser(){
+    if (this.loading) return false;
+
+    this.loading = !this.loading;
+    setTimeout(() => {
+      this.loading = !this.loading;
+    }, 3000);
+  }
 }
