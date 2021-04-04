e8f41d978e70531b038846a7edb637fbdc0c5d5e
Daniel Clavijo Gonzalez <danielclavijo19380@gmail.com>
2021-04-04 21:04:00 +0000

    Reparado problema de la libreria css

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import * as M from 'node_modules/materialize-css/js/materialize.min.js';
+import * as M from 'src/app/services/materialize.js';
 import notie from 'notie';
 import { LoginResponse } from 'src/app/interfaces/login-response';
 import { RegisterResponse } from 'src/app/interfaces/register-response';
@@ -8,164 +8,165 @@ import { ClientService } from 'src/app/services/client.service';
 
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-  constructor(
-    public formB: FormBuilder,
-    private _client: ClientService) { }
+    constructor(
+        public formB: FormBuilder,
+        private _client: ClientService) { }
 
-  public login: boolean = false;
-  private _server: string = 'http://46b28ac89852.ngrok.io';
-  public success: boolean = false;
-  public loading: boolean = false;
-  public error: boolean = false;
-  public formLogin: FormGroup;
-  public formRegister: FormGroup;
+    public login: boolean = false;
+    private _server: string = this._client._server;
+    public success: boolean = false;
+    public loading: boolean = false;
+    public error: boolean = false;
+    public formLogin: FormGroup;
+    public formRegister: FormGroup;
 
-  ngOnInit(): void {
-    M.AutoInit();
-    M.updateTextFields();
-    this.formLogin = this.formB.group({
-      user: ['', Validators.required],
-      password: ['', Validators.required]
-    });
-    this.formRegister = this.formB.group({
-      name: ['', Validators.required],
-      email: ['', Validators.required],
-      user: ['', Validators.required],
-      password: ['', Validators.required]
-    });
-  }
-
-  async loginUser(){
-    const data = {
-      user: this.formLogin.value.user,
-      password: this.formLogin.value.password
-    }
-
-    if (this.formLogin.valid) {
-      this.loading = true;
-
-      this._client.postRequest(`${this._server}/api/v1/user/login`, data).subscribe(
-      ((response: LoginResponse) => {
-        if (response.logged) {
-          this.loading = false;
-          let loggedNotify = new Promise((resolve) => {
-            notie.alert({
-              type: 'info',
-              text: "✔ Inicio correcto",
-              stay: false,
-              time: 2,
-              position: "top"
-            });
-            setTimeout(function(){
-              resolve(true);
-            }, 2000);
-          });
-          loggedNotify.then((e) => {
-            console.log("LOGGED");
-          });
-        }else{
-          this.loading = false;
-          notie.alert({
-            type: 'error',
-            text: "<div>Inicio incorrecto</div>",
-            stay: false,
-            time: 3,
-            position: "top"
-          })
-        }
-      }),
-
-      (error => {
-        console.error(error);
-        notie.alert({
-          type: 'error',
-          text: "<div><span class='font-family-cus-1'>X</span> No hay internet</div>",
-          stay: false,
-          time: 3,
-          position: "top"
+    ngOnInit(): void {
+        M.AutoInit();
+        M.updateTextFields();
+        this.formLogin = this.formB.group({
+            user: ['', Validators.required],
+            password: ['', Validators.required]
         });
-      })
-    )
-    }else{
-      notie.alert({
-        type: 'info',
-        text: "<div>Rellena todos los campos</div>",
-        stay: false,
-        time: 3,
-        position: "top"
-      });
-    }
-  }
-
-  async registerUser(){
-    const data = {
-      name: this.formRegister.value.name,
-      email: this.formRegister.value.email,
-      user: this.formRegister.value.user,
-      password: this.formRegister.value.password
-    }
-
-    if (this.formRegister.valid) {
-      this.loading = true;
-
-      this._client.postRequest(`${this._server}/api/v1/user/register`, data).subscribe(
-      ((response: RegisterResponse) => {
-        if (response.registered) {
-          this.loading = false;
-          let registerNotify = new Promise((resolve) => {
-            notie.alert({
-              type: 'info',
-              text: "✔ registrado",
-              stay: false,
-              time: 2,
-              position: "top"
-            });
-            setTimeout(function(){
-              resolve(true);
-            }, 2000);
-          });
-          registerNotify.then((e) => {
-            this.login = false;
-            console.log("REGISTERED");
-          });
-        }else{
-          this.loading = false;
-          notie.alert({
-            type: 'error',
-            text: "<div>No se ha registrado!, intenta nuevamente</div>",
-            stay: false,
-            time: 3,
-            position: "top"
-          })
-        }
-      }),
-
-      (error => {
-        console.error(error);
-        this.loading = false;
-        notie.alert({
-          type: 'error',
-          text: "<div>No hay internet</div>",
-          stay: false,
-          time: 3,
-          position: "top"
+        this.formRegister = this.formB.group({
+            name: ['', Validators.required],
+            email: ['', Validators.required],
+            user: ['', Validators.required],
+            password: ['', Validators.required]
         });
-      })
-    )
-    }else{
-      notie.alert({
-        type: 'info',
-        text: "<div>Rellena todos los campos</div>",
-        stay: false,
-        time: 3,
-        position: "top"
-      });
     }
-  }
+
+    loginUser() {
+        const data = {
+            user: this.formLogin.value.user,
+            password: this.formLogin.value.password
+        }
+
+        if (this.formLogin.valid) {
+            this.loading = true;
+
+            this._client.postRequest(`${this._server}/api/v1/user/login`, data).subscribe(
+                ((response: LoginResponse) => {
+                    if (response.logged) {
+                        this.loading = false;
+                        let loggedNotify = new Promise((resolve) => {
+                            notie.alert({
+                                type: 'info',
+                                text: "✔ Inicio correcto",
+                                stay: false,
+                                time: 2,
+                                position: "top"
+                            });
+                            setTimeout(function () {
+                                resolve(true);
+                            }, 2000);
+                        });
+                        loggedNotify.then((e) => {
+                            console.log("LOGGED");
+                        });
+                    } else {
+                        this.loading = false;
+                        notie.alert({
+                            type: 'error',
+                            text: "<div>Inicio incorrecto</div>",
+                            stay: false,
+                            time: 3,
+                            position: "top"
+                        })
+                    }
+                }),
+
+                (error => {
+                    console.error(error);
+                    this.loading = false;
+                    notie.alert({
+                        type: 'error',
+                        text: "<div>No hay internet</div>",
+                        stay: false,
+                        time: 3,
+                        position: "top"
+                    });
+                })
+            )
+        } else {
+            notie.alert({
+                type: 'info',
+                text: "<div>Rellena todos los campos</div>",
+                stay: false,
+                time: 3,
+                position: "top"
+            });
+        }
+    }
+
+    registerUser() {
+        const data = {
+            name: this.formRegister.value.name,
+            email: this.formRegister.value.email,
+            user: this.formRegister.value.user,
+            password: this.formRegister.value.password
+        }
+
+        if (this.formRegister.valid) {
+            this.loading = true;
+
+            this._client.postRequest(`${this._server}/api/v1/user/register`, data).subscribe(
+                ((response: RegisterResponse) => {
+                    if (response.registered) {
+                        this.loading = false;
+                        let registerNotify = new Promise((resolve) => {
+                            notie.alert({
+                                type: 'info',
+                                text: "✔ registrado",
+                                stay: false,
+                                time: 2,
+                                position: "top"
+                            });
+                            setTimeout(function () {
+                                resolve(true);
+                            }, 2000);
+                        });
+                        registerNotify.then((e) => {
+                            this.login = false;
+                            console.log("REGISTERED");
+                        });
+                    } else {
+                        this.loading = false;
+                        notie.alert({
+                            type: 'error',
+                            text: "<div>No se ha registrado!, intenta nuevamente</div>",
+                            stay: false,
+                            time: 3,
+                            position: "top"
+                        })
+                    }
+                }),
+
+                (error => {
+                    console.error(error);
+                    this.loading = false;
+                    notie.alert({
+                        type: 'error',
+                        text: "<div>No hay internet</div>",
+                        stay: false,
+                        time: 3,
+                        position: "top"
+                    });
+                })
+            )
+        } else {
+            notie.alert({
+                type: 'info',
+                text: "<div>Rellena todos los campos</div>",
+                stay: false,
+                time: 3,
+                position: "top"
+            });
+        }
+    }
 }
--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -1,59 +1,61 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ClientService {
 
-  constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) { }
 
-  getRequest(route: string, token?:string) {
+    public _server: string = '';
 
-    let config:any = {
-      responseType: "json"
-    }
-    if (token){
-      const header = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-      config["headers"] = header;
-    }
-    console.log(config);
+    getRequest(route: string, token?: string) {
 
-    return this.http.get(route, config);
-  }
-
-  postRequest(route: string, data?:any, token?:string) {
-
-    if (token){
-      let headers = new HttpHeaders({ 'Content-Type': 'application/json','Authorization': `Bearer ${token}` })
-
-      return this.http.post(route, data, {headers});
-    }else{
-      let config:any = {
-      responseType: "json"
+        let config: any = {
+            responseType: "json"
         }
-      return this.http.post(route, data, config);
-    }
-  }
-
-  putRequest(route: string, data?:any, token?:string) {
-
-    if (token){
-      // let config = {
-      //   responseType: "json",
-      //   whitelistedDomains: ['localhost:5000','bdc587af0fe7.ngrok.io','localhost:4200']
-      // }
-      // let headers = new HttpHeaders({ 'Content-Type': 'application/json','Authorization': `Bearer ${token}` })
-      const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`).set('Content-Type', `application/json`)
-      // const headers = { 'Authorization': `Bearer ${token}`, 'Content-Type': 'application/json' };
-
-      return this.http.put(route, data, {headers});
-    }else{
-      let config:any = {
-      responseType: "json"
+        if (token) {
+            const header = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+            config["headers"] = header;
         }
-      return this.http.put(route, data, config);
+        console.log(config);
+
+        return this.http.get(route, config);
     }
-  }
+
+    postRequest(route: string, data?: any, token?: string) {
+
+        if (token) {
+            let headers = new HttpHeaders({ 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` })
+
+            return this.http.post(route, data, { headers });
+        } else {
+            let config: any = {
+                responseType: "json"
+            }
+            return this.http.post(route, data, config);
+        }
+    }
+
+    putRequest(route: string, data?: any, token?: string) {
+
+        if (token) {
+            // let config = {
+            //   responseType: "json",
+            //   whitelistedDomains: ['localhost:5000','bdc587af0fe7.ngrok.io','localhost:4200']
+            // }
+            // let headers = new HttpHeaders({ 'Content-Type': 'application/json','Authorization': `Bearer ${token}` })
+            const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`).set('Content-Type', `application/json`)
+            // const headers = { 'Authorization': `Bearer ${token}`, 'Content-Type': 'application/json' };
+
+            return this.http.put(route, data, { headers });
+        } else {
+            let config: any = {
+                responseType: "json"
+            }
+            return this.http.put(route, data, config);
+        }
+    }
 
 }
