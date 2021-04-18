7a585959aef24dc0eb83bb630e5632ebf9142f89
Daniel Clavijo Gonzalez <danielclavijo19380@gmail.com>
2021-04-18 09:40:59 -0500

    Mejorado sistema de recuperación de contraseña

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -7,7 +7,7 @@ import { RegisterResponse } from 'src/app/interfaces/register-response';
 import { ClientService } from 'src/app/services/client.service';
 import { GoogleClientService } from 'src/app/services/google-client.service';
 import { GoogleClient } from 'src/app/interfaces/google-client';
-import { RecoveryAccount, RecoveryAttempt } from 'src/app/interfaces/recovery-account';
+import { RecoveredStatus, RecoveryAccount, RecoveryAttempt } from 'src/app/interfaces/recovery-account';
 
 
 @Component({
@@ -30,8 +30,20 @@ export class LoginComponent implements OnInit {
     public formLogin: FormGroup;
     public formRegister: FormGroup;
     public formRecoveryPwd: FormGroup;
+    public formRecoveryPwdCode: FormGroup;
+    public formRecoverySetPwd: FormGroup;
     public recoveryPwd: boolean = false;
+    public recoveryPwdCode: boolean = false;
+    public recoveryPwdSetPwd: boolean = false;
     private action: string = null;
+    public timeClock: any = {
+        minute: 0,
+        second: 0,
+        active: false
+    };
+    public recoveryAttemptCode: number = 0;
+    public intervalTimeClock: any;
+    private token_recovery: string = null;
 
     ngOnInit(): void {
         M.AutoInit();
@@ -48,7 +60,15 @@ export class LoginComponent implements OnInit {
         });
         this.formRecoveryPwd = this.formB.group({
             account: ['', Validators.required]
-        })
+        });
+        this.formRecoveryPwdCode = this.formB.group({
+            code: ['', Validators.required]
+        });
+
+        this.formRecoverySetPwd = this.formB.group({
+            password: ['', Validators.required],
+            check_password: ['', Validators.required],
+        });
 
         this.Google.listenInfo().subscribe(
             (info: GoogleClient) => {
@@ -129,7 +149,7 @@ export class LoginComponent implements OnInit {
                         this.action = null;
                         let loggedNotify = new Promise((resolve) => {
                             notie.alert({
-                                type: 'info',
+                                type: 'success',
                                 text: "✔ Inicio correcto",
                                 stay: false,
                                 time: 2,
@@ -277,8 +297,9 @@ export class LoginComponent implements OnInit {
         this.Google.GoogleInit();
     }
 
-    recoveryAccount() {
+    recoveryAccount(): void {
         if (this.formRecoveryPwd.valid) {
+            this.loading = true;
             let account = this.formRecoveryPwd.value.account.toString();
             let typeAccount = '';
 
@@ -293,14 +314,16 @@ export class LoginComponent implements OnInit {
                 type: typeAccount
             };
 
-            this.loading = true;
-
             this._client.postRequest(`${this._server}/user/recovery`, data).subscribe(
                 (res: RecoveryAttempt) => {
                     this.loading = false;
                     console.log(res);
                     if (res.recovering) {
-
+                        this.recoveryPwd = false;
+                        this.recoveryPwdCode = true;
+                        this.recoveryAttemptCode = 1;
+                        this.token_recovery = res.token;
+                        this.clockInit();
                     } else {
                         let registerNotify = new Promise((resolve) => {
                             notie.alert({
@@ -342,5 +365,259 @@ export class LoginComponent implements OnInit {
                 console.log("RECOVERY CANCEL");
             });
         }
+    }
+
+    validateRecoveryAccCode(): void {
+        if (this.formRecoveryPwdCode.valid) {
+            let code = this.formRecoveryPwdCode.value.code;
+            if (code.length == 6) {
+                const data = {
+                    token: this.token_recovery,
+                    code: code
+                }
+                this.loading = true;
+
+                this._client.postRequest(`${this._server}/user/validate/recoverycode`, data).subscribe(
+                    (res: RecoveryAccount) => {
+                        this.loading = false;
+                        if (res.recovered) {
+                            this.token_recovery = res.token;
+                            this.recoveryPwdSetPwd = true;
+                            this.recoveryPwd = false;
+                            this.recoveryPwdCode = false;
+                            if (this.intervalTimeClock) {
+                                clearInterval(this.intervalTimeClock);
+                            };
+                            this.clockInit();
+
+                            let registerNotify = new Promise((resolve) => {
+                                notie.alert({
+                                    type: 'info',
+                                    text: "Codigo validado correctamente",
+                                    stay: false,
+                                    time: 2,
+                                    position: "top"
+                                });
+                                setTimeout(function () {
+                                    resolve(true);
+                                }, 2000);
+                            })
+                            registerNotify.then((e) => {
+                                console.log("RECOVERY SUCCESS");
+                            });
+                        } else {
+                            let registerNotify = new Promise((resolve) => {
+                                notie.alert({
+                                    type: 'error',
+                                    text: "Codigo incorrecto",
+                                    stay: false,
+                                    time: 2,
+                                    position: "top"
+                                });
+                                setTimeout(function () {
+                                    resolve(true);
+                                }, 2000);
+                            })
+                            registerNotify.then((e) => {
+                                console.log("RECOVERY SUCCESS");
+                            });
+                        }
+                    },
+                    (error: any) => {
+                        this.loading = false;
+                        console.log("ERROR");
+                        console.error(error);
+                    }
+                );
+
+            } else {
+                let registerNotify = new Promise((resolve) => {
+                    notie.alert({
+                        type: 'error',
+                        text: "Ingresa un código válido",
+                        stay: false,
+                        time: 2,
+                        position: "top"
+                    });
+                    setTimeout(function () {
+                        resolve(true);
+                    }, 2000);
+                })
+                registerNotify.then((e) => {
+                    console.log("RECOVERY CANCEL");
+                });
+            }
+        } else {
+            let registerNotify = new Promise((resolve) => {
+                notie.alert({
+                    type: 'info',
+                    text: "Rellena el campo",
+                    stay: false,
+                    time: 2,
+                    position: "top"
+                });
+                setTimeout(function () {
+                    resolve(true);
+                }, 2000);
+            })
+            registerNotify.then((e) => {
+                console.log("RECOVERY CANCEL");
+            });
+        }
+    }
+
+    validateChangePwd(): void {
+        if (this.formRecoverySetPwd.valid) {
+            const pwd = this.formRecoverySetPwd.value.password;
+            const check_pwd = this.formRecoverySetPwd.value.check_password;
+
+            if (pwd == check_pwd) {
+                console.log(pwd.length);
+
+                if (pwd.length >= 8) {
+                    this.loading = true;
+                    const data = {
+                        token: this.token_recovery,
+                        pwd: pwd
+                    }
+
+                    this._client.postRequest(`${this._server}/user/validate/changepwd`, data).subscribe(
+                        (res: RecoveredStatus) => {
+                            this.loading = false;
+                            if (res.recovered) {
+                                let registerNotify = new Promise((resolve) => {
+                                    notie.alert({
+                                        type: 'success',
+                                        text: "Contraseña reestablecida correctamente",
+                                        stay: false,
+                                        time: 2,
+                                        position: "top"
+                                    });
+                                    setTimeout(function () {
+                                        resolve(true);
+                                    }, 2000);
+                                })
+                                registerNotify.then((e) => {
+                                    console.log("RECOVERY RECOVERED SUCCESS");
+                                    this.cancelRecoveryPwd();
+                                });
+                            } else {
+                                let registerNotify = new Promise((resolve) => {
+                                    notie.alert({
+                                        type: 'error',
+                                        text: "No se ha podido cambiar la contraseña",
+                                        stay: false,
+                                        time: 2,
+                                        position: "top"
+                                    });
+                                    setTimeout(function () {
+                                        resolve(true);
+                                    }, 2000);
+                                })
+                                registerNotify.then((e) => {
+                                    console.log("RECOVERY ERROR");
+                                });
+                            }
+                        },
+                        (error: any) => {
+                            this.loading = false;
+                            console.log("ERROR");
+                            console.error(error);
+                        }
+                    )
+                } else {
+                    let registerNotify = new Promise((resolve) => {
+                        notie.alert({
+                            type: 'info',
+                            text: "Elije una contraseña más segura",
+                            stay: false,
+                            time: 2,
+                            position: "top"
+                        });
+                        setTimeout(function () {
+                            resolve(true);
+                        }, 2000);
+                    })
+                    registerNotify.then((e) => {
+                        console.log("RECOVERY ERROR");
+                    });
+                }
+
+            } else {
+                let registerNotify = new Promise((resolve) => {
+                    notie.alert({
+                        type: 'info',
+                        text: "Las contraseñas no coinciden",
+                        stay: false,
+                        time: 2,
+                        position: "top"
+                    });
+                    setTimeout(function () {
+                        resolve(true);
+                    }, 2000);
+                })
+                registerNotify.then((e) => {
+                    console.log("RECOVERY ERROR");
+                });
+            }
+
+        } else {
+            let registerNotify = new Promise((resolve) => {
+                notie.alert({
+                    type: 'info',
+                    text: "Rellena los campos",
+                    stay: false,
+                    time: 2,
+                    position: "top"
+                });
+                setTimeout(function () {
+                    resolve(true);
+                }, 2000);
+            })
+            registerNotify.then((e) => {
+                console.log("RECOVERY WAITING...");
+            });
+        }
+    }
+
+    clockInit(): void {
+        this.timeClock.minute = 4;
+        this.timeClock.second = 59;
+        this.timeClock.active = true;
+        this.intervalTimeClock = setInterval(() => {
+            if (this.timeClock.minute == 0 && this.timeClock.second == 0) {
+                this.timeClock.active = false;
+                this.recoveryPwdCode = false;
+                this.recoveryPwdSetPwd = false;
+                this.recoveryPwd = true;
+                setTimeout(() => {
+                    this.recoveryAttemptCode = 0;
+                }, 2500);
+                clearInterval(this.intervalTimeClock)
+            } else {
+                if (this.timeClock.second == 0 && this.timeClock.minute != 0) {
+                    this.timeClock.minute--;
+                    this.timeClock.second = 59;
+                }
+                // setTimeout(() => {
+                //     console.log("Second Clock");
+                // }, 1000);
+                this.timeClock.second--;
+            }
+        }, 1000)
+    }
+
+    cancelRecoveryPwd(): void {
+        if (this.intervalTimeClock != null) {
+            clearInterval(this.intervalTimeClock);
+        }
+        this.recoveryAttemptCode = 0;
+        this.timeClock.active = false;
+        this.timeClock.minute = 0;
+        this.timeClock.second = 0;
+        this.recoveryPwdCode = false;
+        this.recoveryPwd = false;
+        this.recoveryPwdSetPwd = false;
+        this.token_recovery = null;
     }
 }
--- src/app/interfaces/recovery-account.ts
+++ src/app/interfaces/recovery-account.ts
@@ -1,8 +1,13 @@
 export interface RecoveryAccount {
-    recovered: boolean
+    recovered: boolean,
+    token: string
 }
 
 export interface RecoveryAttempt {
     recovering: boolean,
     token: string
+}
+
+export interface RecoveredStatus {
+    recovered: boolean
 }
