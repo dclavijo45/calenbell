048059a074275d1542536147dd546aef18740bee
Daniel Clavijo Gonzalez <danielclavijo19380@gmail.com>
2021-04-21 23:25:38 -0500

    Agregado protección de ruta a el login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { CreateEventComponent } from './components/create-event/create-event.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { HomeGuard } from './guardians/home.guard';
+import { LoginGuard } from './guardians/login-guard';
 
 const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'login', component: LoginComponent },
+    { path: '', component: HomeComponent, canActivate: [HomeGuard] },
+    { path: 'login', component: LoginComponent, canActivate: [LoginGuard] },
     { path: 'crearevento', component: CreateEventComponent },
 ];
 
