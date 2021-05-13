f3dbe29ee7be24207c7f41c29ee8a7dda852e7da
Juan Felipe Motato <juanmotato1@gmail.com>
2021-05-13 15:10:39 +0000

    Agregado cambio de estado de la barra de navegación

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { RootPageStatusService } from 'src/app/services/root-page-status.service';
 
 @Component({
-  selector: 'app-chat',
-  templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.css']
+    selector: 'app-chat',
+    templateUrl: './chat.component.html',
+    styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit {
 
-  constructor() { }
+    constructor(private _rootStatusPage: RootPageStatusService) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        // init status tag
+        this._rootStatusPage.changeRootPageNumberStatus(6);
+    }
+
+    ngOnDestroy(): void {
+        // kill status tag
+        this._rootStatusPage.changeRootPageNumberStatus(null);
+    }
 
 }
