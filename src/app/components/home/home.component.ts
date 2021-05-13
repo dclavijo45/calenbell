1dd0825138ff9012133fbd903ffededa262c6aa2
Juan Felipe Motato <juanmotato1@gmail.com>
2021-05-13 16:23:28 +0000

    Correcciones ortograficas

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { EventsRequest } from 'src/app/interfaces/events-request';
+import { EventsDeleteResponse, EventsRequest } from 'src/app/interfaces/events-request';
 import { ClientService } from 'src/app/services/client.service';
 import { CreateCalendarService } from 'src/app/services/create-calendar.service';
 import { TokenAuthStateService } from 'src/app/services/token-auth-state.service';
 import { InfoCurrentEventService } from 'src/app/services/info-current-event.service';
 import { ControlEventsService } from 'src/app/services/control-events.service';
 import { Router } from '@angular/router';
+import { RootPageStatusService } from 'src/app/services/root-page-status.service';
+import notie from 'notie';
+import Swal from 'sweetalert2/dist/sweetalert2.js';
 
 @Component({
     selector: 'app-home',
@@ -18,19 +21,24 @@ export class HomeComponent implements OnInit {
         private createCalendarService: CreateCalendarService,
         private infoCurrentEventService: InfoCurrentEventService,
         private controlEventsService: ControlEventsService,
-        private router: Router) { }
+        private _auth: TokenAuthStateService,
+        private router: Router,
+        private _rootStatusPage: RootPageStatusService) { }
 
     public _server: string = this._client._server;
     public eventsRequest: EventsRequest[] = [];
     public loading: boolean = true;
     public eventsSelected: number[] = [];
     public lazyLoadCharge: number = this.infoCurrentEventService.lazyLoadCharge;
-    public eventsFiltered: EventsRequest[] = [];
-    public notFoundDataFiltered: boolean = null;
+    public eventsBackup: EventsRequest[] = [];
     public inputSearchValue: string = "";
     public checkBoxSelectAll: boolean = false;
+    private _token: string = localStorage.getItem('token');
 
     ngOnInit(): void {
+        // init tag state
+        this._rootStatusPage.changeRootPageNumberStatus(5);
+
         this.createCalendarService.listenerDetectLoadingChange().subscribe(
             (status: boolean) => {
                 this.loading = status;
@@ -40,6 +48,8 @@ export class HomeComponent implements OnInit {
         this.createCalendarService.listenerEventsRequestChange().subscribe(
             (request: EventsRequest[]) => {
                 this.eventsRequest = request;
+                console.log(request);
+
             }
         );
 
@@ -48,6 +58,11 @@ export class HomeComponent implements OnInit {
                 this.lazyLoadCharge = lazyLoadCharge;
             }
         );
+    }
+
+    ngOnDestroy(): void {
+        // kill status tag
+        this._rootStatusPage.changeRootPageNumberStatus(null);
     }
 
     checkBoxClick(idEvent: number): void {
@@ -82,32 +97,149 @@ export class HomeComponent implements OnInit {
         }
     }
 
-    searchEvents(dataToFilter: string): void {
-        if (this.inputSearchValue.trim().length != 0) {
-            this.eventsFiltered = [];
-            this.eventsRequest.forEach((data) => {
+    searchEvents(dataSearch: string): void {
+        // fix spaces
+        let dataToFilter: string = dataSearch.trim();
+
+        // backup
+        if (this.eventsBackup.length == 0) this.eventsBackup = this.eventsRequest;
+
+        // restore
+        if (this.inputSearchValue.length == 0 && this.eventsBackup.length != 0) {
+            this.eventsRequest = this.eventsBackup;
+            this.eventsBackup = [];
+            console.log("RESTORE");
+            return;
+        }
+
+        this.eventsRequest = [];
+
+        this.eventsBackup.forEach((data) => {
+
+            if (dataToFilter[2] == "/") {
+                let dateDetect: string = data.day.toString() + "/" + data.month.toString() + "/" + data.year.toString();
+
+                dateDetect.includes(dataToFilter) ? this.eventsRequest.push(data) : false;
+
+            } else {
                 if (
-                    data.day.toString().includes(dataToFilter) ||
+                    data.day.toString().includes(dataToFilter.toLowerCase()) ||
                     data.hour.toString().includes(dataToFilter) ||
                     data.month.toString().includes(dataToFilter) ||
                     data.year.toString().includes(dataToFilter) ||
-                    data.title.toString().includes(dataToFilter) ||
-                    data.description.toString().includes(dataToFilter)
+                    data.icon.toString().includes(dataToFilter) ||
+                    data.title.toString().toLowerCase().includes(dataToFilter.toLowerCase()) ||
+                    data.description.toString().toLowerCase().includes(dataToFilter.toLowerCase())
                 ) {
-                    this.eventsFiltered.push(data);
+                    this.eventsRequest.push(data);
                 }
-            });
-            this.eventsFiltered.length == 0
-                ? (this.notFoundDataFiltered = true)
-                : (this.notFoundDataFiltered = false);
-        } else {
-            this.notFoundDataFiltered = false;
-        }
+            }
+
+        });
 
     }
 
-    alert(): void {
-        alert("click");
+    editEvent() {
+        this.eventsRequest.some((data: EventsRequest) => {
+
+            if (data.id.toString().includes(this.eventsSelected[0].toString())) {
+                this.controlEventsService.controlEventData = data;
+
+                this.controlEventsService.actionChangedNow('edit');
+
+                return this.router.navigate(['/controlevento']);
+            }
+        });
+    }
+
+    deleteEvents(): void {
+        if (this.eventsSelected.length > 0) {
+            Swal.fire({
+                title: '¿Está seguro de borrar lo seleccionado?',
+                showDenyButton: true,
+                showCancelButton: false,
+                confirmButtonText: `Sí, borrar`,
+                denyButtonText: `Cancelar`,
+            }).then((result) => {
+
+                if (result.isConfirmed) {
+                    let data: string = this.eventsSelected.toString();
+
+                    this.loading = true;
+
+                    this._client.deleteRequest(`${this._server}/user/manage/events`, data, this._token).subscribe(
+                        (res: EventsDeleteResponse) => {
+                            this.loading = false;
+                            this.eventsSelected = [];
+
+                            if (!res.auth_token) {
+                                this._auth.logout();
+                            };
+
+                            if (res.deleted) {
+
+                                let Notify = new Promise((resolve) => {
+                                    notie.alert({
+                                        type: 'success',
+                                        text: "Borrado correctamente",
+                                        stay: false,
+                                        time: 2,
+                                        position: "top"
+                                    });
+                                    setTimeout(function () {
+                                        resolve(true);
+                                    }, 2000);
+                                })
+                                Notify.then((e) => {
+                                    console.log("DELETE SUCCESS");
+                                });
+
+                                this.createCalendarService.EventsAPIUpdated();
+
+                            } else {
+                                let Notify = new Promise((resolve) => {
+                                    notie.alert({
+                                        type: 'error',
+                                        text: "No se ha podido borrar",
+                                        stay: false,
+                                        time: 2,
+                                        position: "top"
+                                    });
+                                    setTimeout(function () {
+                                        resolve(true);
+                                    }, 2000);
+                                })
+                                Notify.then((e) => {
+                                    console.error("DELETE FAILED");
+                                });
+                            }
+
+                        },
+                        (err: any) => {
+                            this.loading = false;
+                            console.error(err);
+                            let Notify = new Promise((resolve) => {
+                                notie.alert({
+                                    type: 'error',
+                                    text: "Revisa tu conexión a internet",
+                                    stay: false,
+                                    time: 2,
+                                    position: "top"
+                                });
+                                setTimeout(function () {
+                                    resolve(true);
+                                }, 2000);
+                            })
+                            Notify.then((e) => {
+                                console.error("Not internet");
+                            });
+                        }
+                    );
+                }
+            })
+
+
+        }
     }
 
     checkBoxSelectAllClick(): void {
@@ -150,30 +282,16 @@ export class HomeComponent implements OnInit {
 
     }
 
-    // clickToP(id) {
-    //     //this.auth.validateJwt()
-    //     this.listP.some((data) => {
-    //         if (data.id.toString().includes(id)) {
-    //             this.infoProduct.img_product = data.img_product;
-    //             this.infoProduct.price_product = data.price_product;
-    //             this.infoProduct.name_product = data.name_product;
-    //             return (this.infoProduct.description_product =
-    //                 data.description_product);
-    //         }
-    //     });
-    // }   FOR USE IN INFO PRODUCTS
-
-    editEvent() {
-        this.eventsRequest.some((data: EventsRequest) => {
-
-            if (data.id.toString().includes(this.eventsSelected[0].toString())) {
-                this.controlEventsService.controlEventData = data;
-
-                this.controlEventsService.actionEvent = 'edit';
-                this.controlEventsService.actionChangedNow();
-
-                return this.router.navigate(['/controlevento']);
+    viewInfoProduct(id) {
+        this.eventsRequest.some((data) => {
+            if (data.id.toString().includes(id)) {
+                this.infoCurrentEventService.title;
+                this.infoCurrentEventService.hour;
+                this.infoCurrentEventService.data;
+                this.infoCurrentEventService.description;
+                return (this.infoCurrentEventService.typeEvent);
             }
         });
     }
+
 }
