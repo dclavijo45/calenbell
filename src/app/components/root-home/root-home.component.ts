473b6337ff8378d49d31ee76047dc28d8fc566f6
Juan Felipe Motato <juanmotato1@gmail.com>
2021-05-13 15:26:14 +0000

    Agregado template de la vista principal

--- src/app/components/root-home/root-home.component.ts
+++ src/app/components/root-home/root-home.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { RootPageStatusService } from 'src/app/services/root-page-status.service';
 
 @Component({
-  selector: 'app-root-home',
-  templateUrl: './root-home.component.html',
-  styleUrls: ['./root-home.component.css']
+    selector: 'app-root-home',
+    templateUrl: './root-home.component.html',
+    styleUrls: ['./root-home.component.css']
 })
 export class RootHomeComponent implements OnInit {
 
-  constructor() { }
+    constructor(private _rootPageStatus: RootPageStatusService) { }
 
-  ngOnInit(): void {
-  }
+    // styles
+    private counterEffectStyleOpacityView: number = 0;
+    public opacityView: number = 0;
+
+    ngOnInit(): void {
+        // init root page
+        this._rootPageStatus.changeRootPageStatus(true);
+
+        // style effect
+        let intervalOpacity: any = setInterval(() => {
+            this.opacityView += 0.1;
+
+            this.counterEffectStyleOpacityView++;
+
+            this.counterEffectStyleOpacityView == 10 ? clearInterval(intervalOpacity) : true;
+        }, 24);
+    }
+
+    ngOnDestroy(): void {
+        // kill root page
+        this._rootPageStatus.changeRootPageStatus(false);
+    }
 
 }
