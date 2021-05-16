0b31cee48d44fdf5aef3690138f9c42793083847
Daniel Clavijo Gonzalez <danielclavijo19380@gmail.com>
2021-05-16 07:20:24 -0500

    Agregado diseño de vista del chat

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 import { RootPageStatusService } from 'src/app/services/root-page-status.service';
 
 @Component({
@@ -6,18 +6,72 @@ import { RootPageStatusService } from 'src/app/services/root-page-status.service
     templateUrl: './chat.component.html',
     styleUrls: ['./chat.component.css']
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit, AfterViewInit, OnDestroy {
 
     constructor(private _rootStatusPage: RootPageStatusService) { }
 
+    public isChatting: boolean = false;
+
+    // style effect
+    public opacityView: number = 0;
+
     ngOnInit(): void {
+
         // init status tag
         this._rootStatusPage.changeRootPageNumberStatus(6);
+    }
+
+    ngAfterViewInit(): void {
+        // init view style
+        this.initStyleEffect();
     }
 
     ngOnDestroy(): void {
         // kill status tag
         this._rootStatusPage.changeRootPageNumberStatus(null);
+    }
+
+    initStyleEffect(): Promise<boolean> {
+        // style effect
+        return new Promise((resolve) => {
+            let intervalOpacityClear: Promise<boolean> = new Promise((resolve) => {
+                let deleteOpacity = setInterval(() => {
+                    this.opacityView -= 0.1;
+
+                    if (this.opacityView <= 0) {
+                        resolve(true);
+                        clearInterval(deleteOpacity);
+                    }
+                }, 30);
+            });
+
+            intervalOpacityClear.then((res) => {
+
+                let intervalOpacity: Promise<boolean> = new Promise((resolve) => {
+                    let addOpacity = setInterval(() => {
+                        this.opacityView += 0.1;
+
+                        if (this.opacityView >= 1) {
+                            resolve(true);
+                            clearInterval(addOpacity);
+                        }
+                    }, 30);
+                });
+
+                intervalOpacity.then((res) => resolve(true));
+
+            });
+        });
+
+    }
+
+    initChat(): void {
+        let styleInit: Promise<boolean> = this.initStyleEffect();
+
+        styleInit.then((res) => {
+            this.isChatting = true;
+        });
+
     }
 
 }
