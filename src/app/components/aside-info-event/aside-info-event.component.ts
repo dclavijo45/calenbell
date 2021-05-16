ef6783b648bbbe5873d02c94dca5ba4ca63ead0a
Daniel Clavijo Gonzalez <danielclavijo19380@gmail.com>
2021-05-16 07:22:20 -0500

    Agregado cambios de errores ortográficos

--- src/app/components/aside-info-event/aside-info-event.component.ts
+++ src/app/components/aside-info-event/aside-info-event.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { InfoCurrentEventService } from 'src/app/services/info-current-event.service';
 
 @Component({
-  selector: 'app-aside-info-event',
-  templateUrl: './aside-info-event.component.html',
-  styleUrls: ['./aside-info-event.component.css']
+    selector: 'app-aside-info-event',
+    templateUrl: './aside-info-event.component.html',
+    styleUrls: ['./aside-info-event.component.css']
 })
 export class AsideInfoEventComponent implements OnInit {
 
-  constructor() { }
+    constructor(private infoCurrentSrv: InfoCurrentEventService) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+
+    }
 
 }
