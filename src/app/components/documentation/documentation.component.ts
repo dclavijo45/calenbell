6a891bfad3e78a90bd6a80058cca04cb653fc44c
Juan Felipe Motato <juanmotato1@gmail.com>
2021-05-28 10:44:54 +0000

    Se agrego vista de la documentación

--- src/app/components/documentation/documentation.component.ts
+++ src/app/components/documentation/documentation.component.ts
@@ -10,13 +10,17 @@ export class DocumentationComponent implements OnInit {
 
     constructor(private _rootStatusPage: RootPageStatusService) { }
 
+    pdfSource: string = `https://calenbell.web.app/pdf/ManualDeUsuario.pdf`;
+
     ngOnInit(): void {
         this._rootStatusPage.changeRootPageNumberStatus(1);
+        this._rootStatusPage.changeRootPageStatus(true);
     }
 
     ngOnDestroy(): void {
         this._rootStatusPage.changeRootPageStatus(false);
         this._rootStatusPage.changeRootPageNumberStatus(null);
     }
+
 
 }
