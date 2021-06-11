66f3748c7ce99f3afec7d947c4bd7f7f289cd9f7
Daniel Clavijo Gonzalez <danielclavijo19380@gmail.com>
2021-06-11 07:27:40 -0500

    Update: Cambiado interfaz de la respuesta del login

--- src/app/services/token-auth-state.service.ts
+++ src/app/services/token-auth-state.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { LoginResponse } from '../interfaces/login-response';
 import { ClientService } from './client.service';
 
 @Injectable({
@@ -11,7 +12,6 @@ export class TokenAuthStateService {
     constructor(private router: Router,
         private client: ClientService) { }
 
-    private token_jwt: string;
     isLogin = new BehaviorSubject<boolean>(this.checkToken());
 
     public checkToken(): boolean {
@@ -19,45 +19,16 @@ export class TokenAuthStateService {
     }
 
 
-    login(token: string): void {
-        localStorage.setItem('token', token);
+    login(info_profile: LoginResponse): void {
+        localStorage.setItem('token', info_profile.token);
+        localStorage.setItem('info_profile', JSON.stringify(info_profile))
         this.isLogin.next(true);
-    }
-
-    validateJwt() {
-        try {
-            this.token_jwt = localStorage.getItem('token');
-            this.client.postRequest(`${this.client._server}/checkjwt`, {}, this.token_jwt).subscribe(
-                (response: any) => {
-                    let res = JSON.parse(JSON.stringify(response));
-                    if (!res.auth_token) {
-                        this.isLogin.next(false);
-
-                        return false;
-                    } else {
-                        this.isLogin.next(true);
-
-                        return true;
-                    }
-                },
-                (error) => {
-                    console.log(`ERROR Q1 IN GUARD ${error}`);
-                    this.isLogin.next(false);
-
-                    return false;
-                }
-            )
-        } catch (error) {
-            console.log(`ERROR Q2 IN GUARD ${error}`);
-            this.isLogin.next(false);
-
-            return false;
-        }
     }
 
     logout(): void {
         localStorage.removeItem('token');
-        //temp
+        localStorage.removeItem('info_profile');
+
         this.router.navigate(['/login'])
 
         this.isLogin.next(false);
