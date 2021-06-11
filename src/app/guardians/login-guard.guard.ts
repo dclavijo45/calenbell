54e95dfcd75ac08b7c83558c9af4c788fc61bb09
Daniel Clavijo Gonzalez <danielclavijo19380@gmail.com>
2021-06-11 07:04:19 -0500

    Add: Agregado guardian de la ruta de login

---
+++ src/app/guardians/login-guard.guard.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { TokenAuthStateService } from '../services/token-auth-state.service';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class LoginGuard implements CanActivate {
+
+    constructor(private tokenAuth: TokenAuthStateService,
+        private route: Router) { }
+    canActivate(
+        route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+        return new Promise<boolean>((resolve, reject) => {
+            this.tokenAuth.isLoggedIn().subscribe(
+                (login: boolean) => {
+                    if (!login) {
+                        resolve(true);
+                    } else {
+                        this.route.navigate(['/home']);
+                        resolve(false);
+                    }
+                }
+            )
+        });
+    }
+
+}
--- src/app/guardians/login-guard.spec.ts
+++ src/app/guardians/login-guard.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { LoginGuard } from './login-guard';
+import { LoginGuard } from './login-guard.guard';
 
 describe('LoginGuardGuard', () => {
     let guard: LoginGuard;
