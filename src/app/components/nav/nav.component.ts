156a78a9211720bf1ea929839d5116016d0ccea8
Daniel Clavijo Gonzalez <danielclavijo19380@gmail.com>
2021-06-11 07:00:45 -0500

    Update: Cambiado diseño de la barra de navegación, agregado cambio dinámico del color de la barra de navegación, agregado cambio dinámico de barra de navegación para los casos -logueado y -no logueado

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -3,6 +3,10 @@ import { TokenAuthStateService } from 'src/app/services/token-auth-state.service
 import notie from 'notie';
 // import * as M from 'src/app/services/materialize.js';
 import { RootPageStatusService } from 'src/app/services/root-page-status.service';
+import { SocketWebService } from 'src/app/services/socket-web.service';
+import Swal from 'sweetalert2/dist/sweetalert2.js';
+import { InfoProfileService } from 'src/app/services/info-profile.service';
+import { ThemeColorService } from 'src/app/services/theme-color.service';
 
 @Component({
     selector: 'app-nav',
@@ -12,7 +16,7 @@ import { RootPageStatusService } from 'src/app/services/root-page-status.service
 export class NavComponent implements OnInit {
 
     constructor(public _auth: TokenAuthStateService,
-        public _rootPageStatus: RootPageStatusService) { }
+        public _rootPageStatus: RootPageStatusService, public socketWebSvc: SocketWebService, public infoProfileSvc: InfoProfileService, public TC: ThemeColorService) { }
 
     private loggedState: boolean = false;
 
@@ -22,6 +26,17 @@ export class NavComponent implements OnInit {
         this._auth.isLoggedIn().subscribe(
             (login: boolean) => {
                 this.loggedState = login;
+                if (login) {
+                    // set token into localstorage
+                    this.socketWebSvc.connectServerSocket(localStorage.getItem('token'));
+
+                    // set info into profile
+                    this.infoProfileSvc.info_profile = JSON.parse(localStorage.getItem('info_profile'))
+                } else {
+
+                    if (this.socketWebSvc.statusConnection === true) this.socketWebSvc.disconnectServerSocket();
+                };
+
             }
         );
 
@@ -35,24 +50,29 @@ export class NavComponent implements OnInit {
 
     logout(): void {
         if (this.loggedState) {
-            let Notify = new Promise((resolve) => {
-                notie.alert({
-                    type: 'info',
-                    text: "Se ha cerrado sesión correctamente",
-                    stay: false,
-                    time: 2,
-                    position: "top"
-                });
-                setTimeout(function () {
-                    resolve(true);
-                }, 2000);
-            })
-            Notify.then((e) => {
-                console.log("SESSION EXIT SUCCESS");
+            Swal.fire({
+                title: '¿Está seguro que desea cerrar sesión?, se perderan los mensajes de los chats que hayan guardados',
+                showDenyButton: true,
+                showCancelButton: false,
+                confirmButtonText: `Continuar`,
+                denyButtonText: `Cancelar`,
+            }).then((result) => {
+
+                if (result.isConfirmed) {
+                    notie.alert({
+                        type: 'info',
+                        text: "Se ha cerrado sesión correctamente",
+                        stay: false,
+                        time: 2,
+                        position: "top"
+                    });
+
+                    localStorage.removeItem('chats');
+
+                    this._auth.logout();
+                };
             });
+        };
 
-            this._auth.logout();
-        }
     }
-
 }
