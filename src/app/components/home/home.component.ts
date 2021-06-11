659f611742ee2bdb7e6ec340616736f316c17e1e
Daniel Clavijo Gonzalez <danielclavijo19380@gmail.com>
2021-06-11 06:55:10 -0500

    Update: Agregado cambio dinámico del color del fondo del componente, agregado opción de salirse de un evento 'no propietario', cambiado limite de palabras a mostrar

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,6 +9,9 @@ import { Router } from '@angular/router';
 import { RootPageStatusService } from 'src/app/services/root-page-status.service';
 import notie from 'notie';
 import Swal from 'sweetalert2/dist/sweetalert2.js';
+import { LeftEventResponse } from 'src/app/interfaces/left-event-response';
+import { Subscription } from 'rxjs';
+import { ThemeColorService } from 'src/app/services/theme-color.service';
 
 @Component({
     selector: 'app-home',
@@ -23,9 +26,11 @@ export class HomeComponent implements OnInit {
         private controlEventsService: ControlEventsService,
         private _auth: TokenAuthStateService,
         private router: Router,
-        private _rootStatusPage: RootPageStatusService) { }
+        private _rootStatusPage: RootPageStatusService,
+        public TC: ThemeColorService) { }
 
     public _server: string = this._client._server;
+    private subLeftToEvent$: Subscription;
     public eventsRequest: EventsRequest[] = [];
     public loading: boolean = true;
     public eventsSelected: number[] = [];
@@ -48,7 +53,23 @@ export class HomeComponent implements OnInit {
         this.createCalendarService.listenerEventsRequestChange().subscribe(
             (request: EventsRequest[]) => {
                 this.eventsRequest = request;
-                console.log(request);
+
+                if (this.eventsRequest.length > 0) {
+
+                    // set aside info current event
+                    this.infoCurrentEventService.infoEvent.title = this.eventsRequest[0].title
+                    this.infoCurrentEventService.infoEvent.hour = this.eventsRequest[0].hour;
+                    this.infoCurrentEventService.infoEvent.date.day = this.eventsRequest[0].day;
+                    this.infoCurrentEventService.infoEvent.date.month = this.eventsRequest[0].month;
+                    this.infoCurrentEventService.infoEvent.date.year = this.eventsRequest[0].year;
+                    this.infoCurrentEventService.infoEvent.description = this.eventsRequest[0].description;
+                    this.infoCurrentEventService.infoEvent.id = this.eventsRequest[0].id;
+                    this.infoCurrentEventService.infoEvent.icon = this.eventsRequest[0].icon;
+                    this.infoCurrentEventService.infoEvent.typeEvent = this.eventsRequest[0].type_ev;
+
+                    // get participants of current event
+                    this.infoCurrentEventService.getParticipants();
+                };
 
             }
         );
@@ -63,20 +84,39 @@ export class HomeComponent implements OnInit {
     ngOnDestroy(): void {
         // kill status tag
         this._rootStatusPage.changeRootPageNumberStatus(null);
+
+        // reset info current event
+        this.infoCurrentEventService.infoEvent = {
+            title: null,
+            id: null,
+            hour: null,
+            date: {
+                day: null,
+                month: null,
+                year: null
+            },
+            description: null,
+            icon: null,
+            typeEvent: null,
+            participants: [],
+            owner: false
+        };
     }
 
     checkBoxClick(idEvent: number): void {
         if (!this.eventsSelected.includes(idEvent)) {
             this.eventsSelected.push(idEvent);
 
-            this.eventsRequest.some((data) => {
+            this.getEventsOwner().some((data) => {
                 if (data.id.toString().includes(idEvent.toString())) {
                     const index = this.eventsRequest.indexOf(data);
 
-                    this.eventsSelected.length == this.eventsRequest.length ? this.checkBoxSelectAll = true : this.checkBoxSelectAll = false;
+                    if (this.eventsRequest[index].owner) {
+                        this.eventsSelected.length == this.eventsRequest.filter(item => item.owner).length ? this.checkBoxSelectAll = true : this.checkBoxSelectAll = false;
 
+                        return (this.eventsRequest[index].check = true);
+                    };
 
-                    return (this.eventsRequest[index].check = true);
                 }
             });
 
@@ -89,12 +129,19 @@ export class HomeComponent implements OnInit {
                 if (data.id.toString().includes(idEvent.toString())) {
                     const index = this.eventsRequest.indexOf(data);
 
-                    this.eventsSelected.length == this.eventsRequest.length ? this.checkBoxSelectAll = true : this.checkBoxSelectAll = false;
+                    if (this.eventsRequest[index].owner) {
+                        this.eventsSelected.length == this.eventsRequest.filter(item => item.owner).length ? this.checkBoxSelectAll = true : this.checkBoxSelectAll = false;
 
-                    return (this.eventsRequest[index].check = false);
+                        return (this.eventsRequest[index].check = false);
+                    };
+
                 }
             });
         }
+    }
+
+    getEventsOwner(): EventsRequest[] {
+        return this.eventsRequest.filter(item => item.owner);
     }
 
     searchEvents(dataSearch: string): void {
@@ -108,7 +155,7 @@ export class HomeComponent implements OnInit {
         if (this.inputSearchValue.length == 0 && this.eventsBackup.length != 0) {
             this.eventsRequest = this.eventsBackup;
             this.eventsBackup = [];
-            console.log("RESTORE");
+
             return;
         }
 
@@ -140,7 +187,7 @@ export class HomeComponent implements OnInit {
     }
 
     editEvent() {
-        this.eventsRequest.some((data: EventsRequest) => {
+        this.getEventsOwner().some((data: EventsRequest) => {
 
             if (data.id.toString().includes(this.eventsSelected[0].toString())) {
                 this.controlEventsService.controlEventData = data;
@@ -163,55 +210,71 @@ export class HomeComponent implements OnInit {
             }).then((result) => {
 
                 if (result.isConfirmed) {
-                    let data: string = this.eventsSelected.toString();
+                    let find: EventsRequest;
+
+                    let data: string = this.eventsSelected.reduce((acc: number[], item) => {
+                        find = this.eventsRequest.find((event) => event.owner && event.id == item);
+
+                        find ? acc.push(find.id) : true;
+
+                        return acc;
+
+                    }, []).toString()
+
 
                     this.loading = true;
 
                     this._client.deleteRequest(`${this._server}/user/manage/events`, data, this._token).subscribe(
                         (res: EventsDeleteResponse) => {
                             this.loading = false;
-                            this.eventsSelected = [];
 
                             if (!res.auth_token) {
                                 this._auth.logout();
+                                return;
                             };
 
                             if (res.deleted) {
+                                this.eventsSelected = [];
 
-                                let Notify = new Promise((resolve) => {
-                                    notie.alert({
-                                        type: 'success',
-                                        text: "Borrado correctamente",
-                                        stay: false,
-                                        time: 2,
-                                        position: "top"
-                                    });
-                                    setTimeout(function () {
-                                        resolve(true);
-                                    }, 2000);
-                                })
-                                Notify.then((e) => {
-                                    console.log("DELETE SUCCESS");
+                                notie.alert({
+                                    type: 'success',
+                                    text: "Borrado correctamente",
+                                    stay: false,
+                                    time: 2,
+                                    position: "top"
                                 });
+
+                                this.infoCurrentEventService.resetCurrentInfo();
 
                                 this.createCalendarService.EventsAPIUpdated();
 
-                            } else {
-                                let Notify = new Promise((resolve) => {
+                            };
+
+                            if (!res.deleted) {
+                                const reasons = {
+                                    1: () => {
+                                        notie.alert({
+                                            type: 'info',
+                                            text: "Hay otras personas vinculadas en el evento, primero debes quitarlas",
+                                            stay: false,
+                                            time: 2,
+                                            position: "top"
+                                        });
+                                    }
+                                };
+
+                                const showReason = reasons[res.reason] ? reasons[res.reason] : () => {
                                     notie.alert({
                                         type: 'error',
-                                        text: "No se ha podido borrar",
+                                        text: "Hubo un problema interno, por favor contacta soporte",
                                         stay: false,
                                         time: 2,
                                         position: "top"
                                     });
-                                    setTimeout(function () {
-                                        resolve(true);
-                                    }, 2000);
-                                })
-                                Notify.then((e) => {
-                                    console.error("DELETE FAILED");
-                                });
+                                };
+
+                                showReason();
+
                             }
 
                         },
@@ -235,45 +298,47 @@ export class HomeComponent implements OnInit {
                             });
                         }
                     );
-                }
-            })
+                };
 
+            });
 
         }
     }
 
     checkBoxSelectAllClick(): void {
-        if (this.eventsSelected.length == this.eventsRequest.length) {
+        if (this.eventsSelected.length == this.getEventsOwner().length) {
             this.checkBoxSelectAll = false;
 
             this.eventsSelected = [];
 
-            for (let i = 0; i < this.eventsRequest.length; i++) {
+            for (let i = 0; i < this.getEventsOwner().length; i++) {
                 this.eventsRequest[i].check = false;
-                // this.eventsSelected.push(this.eventsRequest[i].id);
-            }
+            };
+
         } else if (this.eventsSelected.length == 0) {
             this.checkBoxSelectAll = true;
 
-            for (let i = 0; i < this.eventsRequest.length; i++) {
+            for (let i = 0; i < this.getEventsOwner().length; i++) {
                 this.eventsRequest[i].check = true;
 
                 this.eventsSelected.push(this.eventsRequest[i].id);
-            }
-        }
+            };
+        };
 
-        console.log(this.eventsSelected);
     }
 
     selectAllCheckboxes(): void {
-        if (this.eventsSelected.length != this.eventsRequest.length) {
+        if (this.eventsSelected.length != this.getEventsOwner().length) {
             this.checkBoxSelectAll = true;
             this.eventsSelected = [];
 
-            for (let i = 0; i < this.eventsRequest.length; i++) {
-                this.eventsRequest[i].check = true;
+            for (let i = 0; i < this.getEventsOwner().length; i++) {
+                if (this.eventsRequest[i].owner) {
+                    this.eventsRequest[i].check = true;
 
-                this.eventsSelected.push(this.eventsRequest[i].id);
+                    this.eventsSelected.push(this.eventsRequest[i].id);
+                }
+
             }
 
             console.log(this.eventsSelected);
@@ -282,16 +347,123 @@ export class HomeComponent implements OnInit {
 
     }
 
-    viewInfoProduct(id) {
-        this.eventsRequest.some((data) => {
-            if (data.id.toString().includes(id)) {
-                this.infoCurrentEventService.title;
-                this.infoCurrentEventService.hour;
-                this.infoCurrentEventService.data;
-                this.infoCurrentEventService.description;
-                return (this.infoCurrentEventService.typeEvent);
-            }
+    viewInfoProduct(id: number) {
+        if (this.infoCurrentEventService.infoEvent.id != id) {
+            this.eventsRequest.some((data) => {
+                if (data.id.toString().includes(id.toString())) {
+                    this.infoCurrentEventService.infoEvent.title = data.title
+                    this.infoCurrentEventService.infoEvent.hour = data.hour;
+                    this.infoCurrentEventService.infoEvent.date.day = data.day;
+                    this.infoCurrentEventService.infoEvent.date.month = data.month;
+                    this.infoCurrentEventService.infoEvent.date.year = data.year;
+                    this.infoCurrentEventService.infoEvent.description = data.description;
+                    this.infoCurrentEventService.infoEvent.id = data.id;
+                    this.infoCurrentEventService.infoEvent.icon = data.icon;
+                    this.infoCurrentEventService.infoEvent.typeEvent = data.type_ev;
+
+                    return (this.infoCurrentEventService.getParticipants())
+                }
+            });
+        }
+    }
+
+    leftToEvent(id_event: number): void {
+        Swal.fire({
+            title: '¿Está seguro de salir del evento?',
+            showDenyButton: true,
+            showCancelButton: false,
+            confirmButtonText: `Continuar`,
+            denyButtonText: `Cancelar`,
+        }).then((result) => {
+
+            if (result.isConfirmed) {
+                this.subLeftToEvent$ ? this.subLeftToEvent$.unsubscribe() : true;
+
+                this.subLeftToEvent$ = this._client.deleteRequest(`${this._server}/user/participantsevent/${id_event}/`, "", localStorage.getItem('token')).subscribe(
+                    (response: LeftEventResponse) => {
+                        if (!response.auth_token) {
+                            this._auth.logout();
+                            return;
+                        };
+
+                        if (response.left) {
+                            notie.alert({
+                                type: 'success',
+                                text: "Has salido correctamente del evento",
+                                stay: false,
+                                time: 3,
+                                position: "top"
+                            });
+
+                        };
+
+                        if (!response.left) {
+                            const reasons = {
+                                1: () => {
+                                    notie.alert({
+                                        type: 'error',
+                                        text: "Evento no válido",
+                                        stay: false,
+                                        time: 2,
+                                        position: "top"
+                                    });
+                                },
+
+                                2: () => {
+                                    notie.alert({
+                                        type: 'info',
+                                        text: "El evento no existe",
+                                        stay: false,
+                                        time: 2,
+                                        position: "top"
+                                    });
+                                },
+
+                                3: () => {
+                                    notie.alert({
+                                        type: 'error',
+                                        text: "El evento no es de tipo grupo",
+                                        stay: false,
+                                        time: 2,
+                                        position: "top"
+                                    });
+                                },
+
+                                4: () => {
+                                    notie.alert({
+                                        type: 'info',
+                                        text: "No haces parte del evento",
+                                        stay: false,
+                                        time: 2,
+                                        position: "top"
+                                    });
+                                }
+                            };
+
+                            const showNotificationError = reasons[response.reason] ? reasons[response.reason] : () => {
+                                notie.alert({
+                                    type: 'error',
+                                    text: "Ha ocurrido un problema, contacta soporte",
+                                    stay: false,
+                                    time: 2,
+                                    position: "top"
+                                });
+                            };
+
+                            showNotificationError();
+                        };
+
+                        this.createCalendarService.EventsAPIUpdated();
+                    },
+                    (error) => {
+                        console.log("***ERROR***");
+                        console.error(error);
+
+                    }
+                );
+            };
         });
+
     }
 
 }
