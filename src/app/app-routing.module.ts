f61253b1ae47da1f3b095d5eec23f86836a1d150
Daniel Clavijo Gonzalez <danielclavijo19380@gmail.com>
2021-06-11 06:42:06 -0500

    Update: Eliminado ruta de "Why us?", agregado guardian para la ruta 'chat', agregado guardian para la ruta 'home'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,20 +8,18 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RootHomeComponent } from './components/root-home/root-home.component';
 import { UserConfigComponent } from './components/user-config/user-config.component';
-import { WhyUsComponent } from './components/why-us/why-us.component';
 import { HomeGuard } from './guardians/home.guard';
-import { LoginGuard } from './guardians/login-guard';
+import { LoginGuard } from './guardians/login-guard.guard';
 
 const routes: Routes = [
     { path: '', component: RootHomeComponent },
     { path: 'home', component: HomeComponent, canActivate: [HomeGuard] },
     { path: 'login', component: LoginComponent, canActivate: [LoginGuard] },
-    { path: 'controlevento', component: ManageEventComponent },
-    { path: 'chat', component: ChatComponent },
+    { path: 'controlevento', component: ManageEventComponent, canActivate: [HomeGuard] },
+    { path: 'chat', component: ChatComponent, canActivate: [HomeGuard] },
     { path: 'documentation', component: DocumentationComponent },
-    { path: 'why', component: WhyUsComponent },
     { path: 'contact', component: ContactUsComponent },
-    { path: 'settings', component: UserConfigComponent },
+    { path: 'settings', component: UserConfigComponent, canActivate: [HomeGuard] },
 ];
 
 @NgModule({
